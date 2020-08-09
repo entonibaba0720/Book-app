b4b6f7b7ea2c2b32756a59eb590eadd6dcf36445
entonibaba0720 <entonibaba0720@gmail.com>
2020-08-09 19:42:03 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,17 +5,22 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 /* ROUTES */
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
+const authorRouter = require("./routes/authors");
+app.use("/authors", authorRouter);
+
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
-app.use(express.static("public"));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
